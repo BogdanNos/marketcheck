b207b3f30989db754f37fe200b2d81d53c0c0b16
Bogdan <72023861+BogdanNos@users.noreply.github.com>
2024-03-29 18:53:35 +0500

    Парсинг по маркетплейсу

--- background.js
+++ background.js
@@ -1,25 +1,77 @@
-// Get the reference to the HTML body element
-const WebPage = `
-<div style="background-color:white; width: 300px; height:100px; z-index:999; position:fixed; right:0;">
-    <header>
-        <h1>МАРКЕТЧЕК</h1>
-    </header>
-    <main>
-        <section class="best-offers">
-            <h2>Лучшие предложения</h2>
-            <p>
-                <a href="#">По возрастанию цены</a> (от 126.000 рублей)
-            </p>
-            <p>
-                <a href="#">По популярности на WB</a> (от 26.000 рублей)
-            </p>
-        </section>
-    </main>
-</div>
-`;
-window.onload=function(){
+let currentUrl = location.href;
+let isRunning = false;
+let rule = {"wildberries.ru" : {"tag" : "detail", "selector" : 'h1[class="product-page__title"]'},
+            "megamarket.ru" : {"tag" : "details", "selector" : 'h1[itemprop="name"]'},
+            "ozon.ru" : {"tag" : "product", "selector" : 'div[data-widget="webProductHeading"]'},
+            "market.yandex.ru" : {"tag" : "product", "selector" : 'h1[data-additional-zone="title"]'}};
+let domain = window.location.hostname.replace("www.","");
 
+window.addEventListener("load", function(){
+    if (currentUrl.includes(rule[domain]["tag"]) && currentUrl.includes(domain)){
+        runExtension();
+    }
+    else
+    {
+        setInterval(() => {
+            if (location.href !== currentUrl) 
+            {   
+                currentUrl = location.href;
+
+                if (document.querySelector("#market-check-extension")){
+                    document.getElementById("market-check-extension").remove();
+                }
+
+                if (currentUrl.includes(rule[domain]["tag"])){
+                    console.log("PREANADNSDNAS");
+                    runExtension();
+                } 
+            }
+        }, 500);
+    }
+});
+
+function runExtension(){
     const body = document.body;
-    body.insertAdjacentHTML('afterbegin', WebPage);
+    const location = window.location.href;
+    let name = "";
+    if (location.includes(domain) && location.includes(rule[domain]["tag"])){
+        isRunning = true;
+        onElementAvailable(rule[domain]["selector"], () => {
+            name = document.querySelector(rule[domain]["selector"]).textContent;
+            body.insertAdjacentHTML('afterbegin', generateHtml(name));
+        });
+    }
 }
-// Add a paragraph element after the existing content
+
+//Функция ожидания загрузки элемента
+function onElementAvailable(selector, callback) {
+    const observer = new MutationObserver(mutations => {
+      if (document.querySelector(selector)) {
+        observer.disconnect();
+        callback();
+      }
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
+}
+
+function generateHtml(name){
+    return `
+    <div id="market-check-extension" style="background-color:white; width: 300px; height:600px; z-index:999999; position:fixed; right:0;">
+        <header>
+            <h1>МАРКЕТЧЕК</h1>
+        </header>
+        <main>
+            <section class="best-offers">
+                <h2>${name}</h2>
+                <h2>Лучшие предложения</h2>
+                <p>
+                    <a href="#">По возрастанию цены</a> (от 126.000 рублей)
+                </p>
+                <p>
+                    <a href="#">По популярности на WB</a> (от 26.000 рублей)
+                </p>
+            </section>
+        </main>
+    </div>
+    `
+}
