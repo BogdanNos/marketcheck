086c35282c4d5270608757306b12c1af63388cca
Bogdan <72023861+BogdanNos@users.noreply.github.com>
2024-03-30 18:03:55 +0500

    Update background.js

--- background.js
+++ background.js
@@ -53,23 +53,102 @@ function onElementAvailable(selector, callback) {
     });
     observer.observe(document.body, { childList: true, subtree: true });
 }
-
-function generateHtml(name){
+function getStyles(){
     return `
-    <div id="market-check-extension" style="background-color:white; width: 300px; height:600px; z-index:999999; position:fixed; right:0;">
+    #market-check-extension {
+        background-color:white;
+        width: 400px;
+        
+        z-index:999999;
+        position:fixed;
+        right:0; 
+    }
+
+    #market-check-extension header{
+        display: flex;
+        justify-content: space-between;
+        padding: 5px 40px 5px 40px;
+        background-color:#058ED9
+    }
+
+    #market-check-extension main{
+        padding-left:40px;
+        padding-right:40px;
+    }
+
+    #market-check-extension header h1 {
+        font-family: 'Roboto';
+        font-weight: bold;
+        font-size: 20px;
+      }
+    #market-check-extension main h2 {
+        font-family: 'Roboto';
+        font-size: 14px;
+      }
+    
+    .form_toggle {
+        display: inline-block;
+        overflow: hidden;
+    }
+    .form_toggle-item {
+        float: left;
+        display: inline-block;
+    }
+    .form_toggle-item input[type=radio] {
+        display: none;
+    }
+    .form_toggle-item label {
+        display: inline-block;
+        padding: 0px 15px;   
+        line-height: 34px;    
+        border: 1px solid #999;
+        border-right: none;
+        cursor: pointer;
+        user-select: none;   
+    }
+     
+    .form_toggle .item-1 label {
+        border-radius: 6px 0 0 6px;
+    }
+    .form_toggle .item-2 label {
+        border-radius: 0 6px 6px 0;
+        border-right: 1px solid #999;
+    }
+     
+    .form_toggle .item-1 input[type=radio]:checked + label {
+        background: #D9D9D9;
+    }
+    .form_toggle .item-2 input[type=radio]:checked + label {
+        background: #D9D9D9;
+    }  
+      
+    `
+}
+function generateHtml(name){
+    style = getStyles()
+    return `
+    <style>
+    ${style}
+    </style>
+    <div id="market-check-extension">
         <header>
             <h1>МАРКЕТЧЕК</h1>
+            <button></button>
         </header>
-        <main>
+        <main >
             <section class="best-offers">
                 <h2>${name}</h2>
                 <h2>Лучшие предложения</h2>
-                <p>
-                    <a href="#">По возрастанию цены</a> (от 126.000 рублей)
-                </p>
-                <p>
-                    <a href="#">По популярности на WB</a> (от 26.000 рублей)
-                </p>
+                <div class="form_toggle">
+                    <div class="form_toggle-item item-1">
+                        <input id="fid-1" type="radio" name="radio"  checked>
+                        <label for="fid-1">По возрастанию</label>
+                    </div>
+                    <div class="form_toggle-item item-2">
+                        <input id="fid-2" type="radio" name="radio">
+                        <label for="fid-2">По популярности</label>
+                    </div>
+                </div>
             </section>
         </main>
     </div>
